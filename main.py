b8247a6f02f000b93bdda09970e36cf1e07c9876
suk-6 <me@suk.kr>
2023-08-05 19:46:55 +0900

    test

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-app.run(debug=True, host='0.0.0.0', port=3000)
+@app.route('/info/<infoNum>')
+def info(infoNum):
+    return render_template('info.html', infoNum=infoNum)
+
+app.run(debug=True, host='0.0.0.0', port=30000)
